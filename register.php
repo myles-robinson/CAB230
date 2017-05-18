07074ab01fe3e9c8dbc3a63fa42f3a907f216f44
vgutierrez9 <vguti9@gmail.com>
2017-05-18 15:44:29 +1000

    Side by side Login and register on the register page

--- register.php
+++ register.php
@@ -6,9 +6,6 @@
 	Vanessa Gutierrez   N9890394
 	Luke Martin         N9452362
 -->
-<?php require "./header.inc"; ?>
-<?php require "./register.inc"; ?>
-<?php echo "<link rel='stylesheet' type='text/css' href='./stylesheet.css' />"; ?>
 <html>
 
 	<!-- HEAD -->
@@ -21,29 +18,43 @@
 
 	<!-- BODY -->
 	<body>
-
+		<?php require "./header.inc"; ?>
+		<?php require "./register.inc"; ?>
 		<!-- CONTENT -->
-		<div id="registerContent" id="regoform">
-			<br><br><br><br><br>
-			<form action ="register.php" method="post">
-				<h2 style="font-size: 40px"><b>Register an account with ParkFindr:</b></h2>
-				<br><br><br>
-				<input type="text" name="first_name" id="registerInputHalf" placeholder="First Name" onfocus="this.placeholder = ''" required />
-				<input type="text" name="last_name" id="registerInputHalf" placeholder="Last Name" required />
-				<br><br><br>
-				<input type="email" name="email" id="registerInput" placeholder="Email Address" required />
-				<br><br><br>
-				<input type="email" name="confirmemail" id="registerInput" placeholder="Confirm Email" required />
-				<br><br><br>
-				<input type="password" name="password" id="registerInputHalf" placeholder="Password" required />
-				<input type="password" name="confirmpw" id="registerInputHalf" placeholder="Confirm Password" required />
+
+		<div id="formContainer">
+		<!-- Login form -->
+		<div class="registerContent">
+			<form action="register.php" method="post">
+				<h2 class="registerHeading"><b>Already have a ParkFindr account? Log in here.</b></h2>
+				<br>
+        <input class="registerInput" id="registerInputHalf" type="email" name="email" placeholder="Email" onfocus="this.placeholder = ''" required/>
 				<br><br>
-				<input type="submit" name="submit" value="Submit" id="submitButton">
+				<input class="registerInput" type="password" class="registerInput" name="password" placeholder="Password" onfocus="this.placeholder = ''" required/>
+				<br><br>
+				<input class="submitButton" name="submit" type="submit" value="Submit" />
 			</form>
 		</div>
-
-		<br><br><br>
-
+		<br>
+		<div class="registerContent">
+			<!-- Registration Form -->
+			<form action ="register.php" method="post">
+				<h2 class="registerHeading"><b>Register an account with ParkFindr:</b></h2>
+				<br>
+				<input type="text" name="first_name" class="registerInputHalf" placeholder="First Name" onfocus="this.placeholder = ''" required />
+				<input type="text" name="last_name" class="registerInputHalf" placeholder="Last Name" required />
+				<br><br>
+				<input type="email" name="email" class="registerInput" placeholder="Email Address" required />
+				<br><br>
+				<input type="email" name="confirmemail" class="registerInput" placeholder="Confirm Email" required />
+				<br><br>
+				<input type="password" name="password" class="registerInputHalf" placeholder="Password" required />
+				<input type="password" name="confirmpw" class="registerInputHalf" placeholder="Confirm Password" required />
+				<br><br>
+				<input type="submit" name="submit" value="Submit" class="submitButton">
+			</form>
+		</div>
+	</div>
 	<!-- FOOTER -->
 		<div id="footer">
 			CAB230 2017 Semester 1 Project
